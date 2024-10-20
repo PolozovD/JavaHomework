68fa83a4ad839d0e85da8f6fddd260f628d0152c
Dmitriy <dpolozov86@gmail.com>
2024-10-20 19:48:05 +0300

    Homework6

---
+++ src/homework6/Good.java
@@ -0,0 +1,51 @@
+package homework6;
+
+import java.util.Objects;
+
+public class Good {
+    private int ram; // ОЗУ в ГБ
+    private int hdd; // Объем ЖД в ГБ
+    private String os; // Операционная система
+    private String color; // Цвет
+    public Good(int ram, int hdd, String os, String color) {
+        this.ram = ram;
+        this.hdd = hdd;
+        this.os = os;
+        this.color = color;
+    }
+    public int getRam() {
+        return ram;
+    }
+    public int getHdd() {
+        return hdd;
+    }
+    public String getOs() {
+        return os;
+    }
+    public String getColor() {
+        return color;
+    }
+
+    @Override
+    public String toString() {
+        return "module6.Laptop{" +
+                "RAM=" + ram + " GB" +
+                ", HDD=" + hdd + " GB" +
+                ", OS='" + os + '\'' +
+                ", Color='" + color + '\'' +
+                '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Good laptop = (Good) o;
+        return ram == laptop.ram && hdd == laptop.hdd &&
+                os.equals(laptop.os) && color.equals(laptop.color);
+    }
+    @Override
+    public int hashCode() {
+        return Objects.hash(ram, hdd, os, color);
+    }
+}
+
